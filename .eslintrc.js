248503f16aae69e3a3f61cf67ed8a735103e8797
Steve Richards <steve.richards@gmail.com>
2018-08-28 20:51:53 -0700

    Create eslintrc and eslintignore files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  rules: {
+    'arrow-parens': 0,
+    'import/prefer-default-export': 0,
+    'no-plusplus': 0,
+    'no-prototype-builtins': 0,
+    'max-len': ['error', { code: 80, ignoreTrailingComments: true }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
