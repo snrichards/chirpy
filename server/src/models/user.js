57a5deeae6423ca348e8ed8a39fab36e6b080347
Steve Richards <steve.richards@gmail.com>
2018-08-30 02:25:02 -0700

    Create user model

---
+++ server/src/models/user.js
@@ -0,0 +1,35 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        min: 7,
+      },
+    },
+  });
+
+  User.associate = (models) => User.hasMany(models.Chirp);
+
+  return User;
+};
+
+module.exports = user;
