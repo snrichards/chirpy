8c9eb89daa2dd953588aeed27fe3f5e06fdfe000
Steve Richards <steve.richards@gmail.com>
2018-08-31 04:11:58 -0700

    Create resolver to verify user is owner of chirp

--- server/src/resolvers/authorization.js
+++ server/src/resolvers/authorization.js
@@ -4,6 +4,17 @@ const { skip } = require('graphql-resolvers');
 const isAuthenticated = (parent, args, { currentUser }) =>
   currentUser ? skip : new ForbiddenError('Must be signed in.');
 
+const isChirpOwner = async (parent, { id }, { currentUser, models }) => {
+  const chirp = await models.Chirp.findById(id);
+
+  if (chirp.userId !== currentUser.id) {
+    throw new ForbiddenError('Your are not authorized to edit this chirp.');
+  }
+
+  return skip;
+};
+
 module.exports = {
   isAuthenticated,
+  isChirpOwner,
 };
