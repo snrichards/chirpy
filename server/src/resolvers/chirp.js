1b16e3583ea2de68cf64815a733dcee82ee4b307
Steve Richards <steve.richards@gmail.com>
2018-08-31 03:23:15 -0700

    Update createChirp resolver to check for user authentication

--- server/src/resolvers/chirp.js
+++ server/src/resolvers/chirp.js
@@ -1,14 +1,20 @@
+const { combineResolvers } = require('graphql-resolvers');
+const { isAuthenticated } = require('./authorization');
+
 module.exports = {
   Query: {
     chirps: async (parent, args, { models }) => models.Chirp.findAll(),
     chirp: async (parent, { id }, { models }) => models.Chirp.findById(id),
   },
   Mutation: {
-    createChirp: async (parent, { text }, { currentUser, models }) =>
-      models.Chirp.create({
-        text,
-        userId: currentUser.id,
-      }),
+    createChirp: combineResolvers(
+      isAuthenticated,
+      async (parent, { text }, { currentUser, models }) =>
+        models.Chirp.create({
+          text,
+          userId: currentUser.id,
+        }),
+    ),
   },
   Chirp: {
     user: async (chirp, args, { models }) => models.User.findById(chirp.userId),
