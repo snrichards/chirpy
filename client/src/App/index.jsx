96d71b5b0f670af3df202867b4f39490bc843d39
Steve Richards <steve.richards@gmail.com>
2018-09-04 18:15:37 -0700

    Add Navigation and SignUp components to app

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SignIn from '../SignIn';
+import SignUp from '../SignUp';
+import Navigation from '../Navigation';
 
 const App = () => (
   <Router>
     <div>
       <h1>Chirpy</h1>
+      <Navigation />
 
       <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
     </div>
   </Router>
 );
