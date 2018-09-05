ed4078a3eeaef2b6c84304d04fe806ffaa5751b0
Steve Richards <steve.richards@gmail.com>
2018-09-04 17:00:17 -0700

    Create isAuthenticated utils function

---
+++ client/src/utils/index.js
@@ -0,0 +1,26 @@
+import decode from 'jwt-decode';
+
+const isTokenExpired = (token) => {
+  const decoded = decode(token);
+
+  if (!decoded) {
+    return false;
+  }
+
+  const date = new Date(0);
+  date.setUTCSeconds(decoded.exp);
+
+  const offsetSeconds = 0;
+
+  return !(date.valueOf() > new Date().valueOf() + offsetSeconds * 1000);
+};
+
+export const isAuthenticated = () => {
+  const token = localStorage.getItem('chirpyToken');
+
+  if (token) {
+    return !isTokenExpired(token);
+  }
+
+  return false;
+};
