428adf51fbb42c3938465514d441aa8a6aa339b7
Steve Richards <steve.richards@gmail.com>
2018-09-04 20:26:07 -0700

    Add routes for 404 and profile components. Redirect from '/' to profile or signup depending on if user is signed in

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import SignIn from '../SignIn';
 import SignUp from '../SignUp';
 import Navigation from '../Navigation';
+import Profile from '../Profile';
+import PrivateRoute from '../PrivateRoute';
+import { isAuthenticated } from '../utils';
+import FourOhFour from '../FourOhFour';
 
 const App = () => (
   <Router>
@@ -11,9 +20,23 @@ const App = () => (
         <h1>Chirpy</h1>
         <Navigation />
       </header>
-
-      <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/signup" component={SignUp} />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() =>
+            isAuthenticated() ? (
+              <Redirect to="/profile" />
+            ) : (
+              <Redirect to="/signup" />
+            )
+          }
+        />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <PrivateRoute exact path="/profile" component={Profile} />
+        <Route component={FourOhFour} />
+      </Switch>
     </div>
   </Router>
 );
